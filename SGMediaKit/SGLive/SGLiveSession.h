95b1062fe659bc999354da60aeabbe61a885a7ad
Single <musicman_leehom@126.com>
2016-11-30 12:11:37 +0800

    SGLiveSession

---
+++ SGMediaKit/SGLive/SGLiveSession.h
@@ -0,0 +1,13 @@
+//
+//  SGLiveSession.h
+//  SGMediaKit
+//
+//  Created by Single on 30/11/2016.
+//  Copyright © 2016 single. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SGLiveSession : NSObject
+
+@end
---
+++ SGMediaKit/SGLive/SGLiveSession.m
@@ -0,0 +1,13 @@
+//
+//  SGLiveSession.m
+//  SGMediaKit
+//
+//  Created by Single on 30/11/2016.
+//  Copyright © 2016 single. All rights reserved.
+//
+
+#import "SGLiveSession.h"
+
+@implementation SGLiveSession
+
+@end
